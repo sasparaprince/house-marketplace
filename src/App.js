7fe648b9e23b44abc131c037204bca84227053ab
prince saspara <princesaspara46@gmail.com>
2023-08-25 18:39:08 +0530

    Add Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/Offers' element={<Offers />} />
+          <Route path='/Profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/ForgotPassword' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  ) 
 }
 
 export default App
