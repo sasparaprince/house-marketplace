804c6b86d65dc180b7be1c90375d933b72844cbf
prince saspara <princesaspara46@gmail.com>
2023-09-09 21:28:27 +0530

    Listing Details

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Link,useNavigate, useParams } from 'react-router-dom'
-import {getDoc,doc} from 'firebase/firestore'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
@@ -13,13 +13,13 @@ const Listing = () => {
     const [loading, setLoading] = useState(true)
     const [shareLinkCopied, setShareLinkCopied] = useState(null)
 
-    const navigate =useNavigate()
+    const navigate = useNavigate()
     const params = useParams()
     const auth = getAuth()
 
-    useEffect(()=>{
-        const fetchListing =async() => {
-            const docRef = doc(db, 'listings',params.listingId)
+    useEffect(() => {
+        const fetchListing = async () => {
+            const docRef = doc(db, 'listings', params.listingId)
             const docSnap = await getDoc(docRef)
 
             if (docSnap.exists()) {
@@ -29,10 +29,65 @@ const Listing = () => {
             }
         }
         fetchListing()
-    },[navigate,params.listingId])
+    }, [navigate, params.listingId])
+
+    if (loading) {
+        return <spinner />
+    }
 
     return (
-        <div>Listing</div>
+        <main>
+            {/* slider */}
+            <div className="shareIconDiv" onClick={() => {
+                navigator.clipboard.writeText(window.location.href)
+                setShareLinkCopied(true)
+                setTimeout(() => {
+                    setShareLinkCopied(false)
+                }, 2000)
+            }
+            }>
+                <img src={shareIcon} alt="" />
+            </div>
+
+            {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
+            <div className="listingDetails">
+                <p className="listingName">
+                    {listing.name} - ₹{listing.offer ? listing.discountedPrice
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                        : listing.regularPrice
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                </p>
+                <p className="listingLocation">{listing.location}</p>
+                <p className="listingType">
+                    For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+                </p>
+                {listing.offer && (
+                    <p className="discountPrice">
+                        ₹{listing.regularPrice - listing.discountedPrice} Discount
+
+                    </p>
+                )}
+                <ul className='listingDetailsList'>
+                    <li>
+                        {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'} 
+                    </li>
+                    <li>
+                        {listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'} 
+                    </li>
+                    <li>{listing.parking && 'Parking Spot'}</li>
+                    <li>{listing.furnished && 'furnished'}</li>
+                </ul>
+                <p className="listingLocationTitle">Location</p>
+
+                {/* MAP  */}
+
+                {auth.currentUser?.uid !== listing.userRef && (
+                    <Link to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className='primaryButton'>Contact Landlord</Link>
+                )}
+            </div>
+        </main>
     )
 }
 
